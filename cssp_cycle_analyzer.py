6d793ebaa524757b1c3a15aac10655023ae2250c
Antoine Missue <antoine.missue@clearsy.com>
2025-02-21 16:39:25 +0100

    Add some script and the figures from the data

--- cssp_cycle_analyzer.py
+++ cssp_cycle_analyzer.py
@@ -1,52 +1,92 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the CSV file
-df = pd.read_csv('1IO_1blinking/cssp_cycle_times_1_1blink.csv')
+def cssp_cycle_analyzer(file_path, scenario, save_bar, save_box):
+    state_dict = {
+        '0': 'Wait_Local_Write_State',
+        '1': 'Wait_Global_Read_State',
+        '2': 'Wait_External_Write_State',
+        'RW': 'Complete Read/Write Cycle'
+    }
+    # Load the CSV file
+    df = pd.read_csv(file_path)
+
+    # General analysis of cycle times
+    general_stats = df['Cycle Time ms'].describe()
+
+    # Analysis of cycle times by cycle state
+    state_stats = df.groupby('Cycle State')['Cycle Time ms'].describe()
 
 
-# General analysis of cycle times
-general_stats = df['Cycle Time ms'].describe()
+    # Print the results
+    # print("General Statistics of Cycle Times:")
+    # print(general_stats)
 
-# Analysis of cycle times by cycle state
-state_stats = df.groupby('Cycle State')['Cycle Time ms'].describe()
+    print("\nStatistics of Cycle Times by Cycle State:")
+    print(state_stats)
+    total_string = ""
+    for state in df['Cycle State'].unique():
+        state_data = df[df['Cycle State'] == state]['Cycle Time ms']
+        min_time = state_data.min()
+        avg_time = state_data.mean()
+        std_time = state_data.std()
+        max_time = state_data.max()
+        print(f"Cycle State {state_dict[state]}: {min_time:.2f} / {avg_time:.2f}±{std_time:.2f} / {max_time:.2f}")
+        total_string += f" {min_time:.2f} / {avg_time:.2f}±{std_time:.2f} / {max_time:.2f} &"
+    print(total_string)
 
+    # print("\nHighest cycle times:")
+    # for state in df['Cycle State'].unique():
+    #     print(f"\nTop 10 highest cycle times for Cycle State {state}:")
+    #     print(df[df['Cycle State'] == state].nlargest(10, 'Cycle Time ms'))
 
-# Print the results
-print("General Statistics of Cycle Times:")
-print(general_stats)
+    # print("\n Sum of cycle times:")
+    # print(df.groupby('Cycle State')['Cycle Time ms'].sum())
 
-print("\nStatistics of Cycle Times by Cycle State:")
-print(state_stats)
-
-
-print("\nHighest cycle times:")
-for state in df['Cycle State'].unique():
-    print(f"\nTop 10 highest cycle times for Cycle State {state}:")
-    print(df[df['Cycle State'] == state].nlargest(10, 'Cycle Time ms'))
-
-print("\n Sum of cycle times:")
-print(df.groupby('Cycle State')['Cycle Time ms'].sum())
-
-# Plot statistics for each cycle state separately
-for state in df['Cycle State'].unique():
+    # Plot statistics for each cycle state separately
+    # for state in df['Cycle State'].unique():
+    #     plt.figure(figsize=(10, 6))
+    #     subset = df[df['Cycle State'] == state]
+    #     plt.hist(subset['Cycle Time ms'], bins=30, alpha=0.7, label=f'State {state}')
+    #     plt.title(f'Distribution of Cycle Times for Cycle State {state}')
+    #     plt.xlabel('Cycle Time (ms)')
+    #     plt.ylabel('Frequency')
+    #     plt.legend()
+    #     plt.grid(True)
+    #     plt.show()
+    # Plot statistics by cycle state
     plt.figure(figsize=(10, 6))
-    subset = df[df['Cycle State'] == state]
-    plt.hist(subset['Cycle Time ms'], bins=30, alpha=0.7, label=f'State {state}')
-    plt.title(f'Distribution of Cycle Times for Cycle State {state}')
+    for state in df['Cycle State'].unique():
+        subset = df[df['Cycle State'] == state]
+        plt.hist(subset['Cycle Time ms'], bins=30, alpha=0.7, label=f'{state_dict[state]}')
+    plt.title(f'{scenario}: Distribution of Cycle Times by Cycle State')
     plt.xlabel('Cycle Time (ms)')
     plt.ylabel('Frequency')
     plt.legend()
     plt.grid(True)
-    plt.show()
-# Plot statistics by cycle state
-plt.figure(figsize=(10, 6))
-for state in df['Cycle State'].unique():
-    subset = df[df['Cycle State'] == state]
-    plt.hist(subset['Cycle Time ms'], bins=30, alpha=0.7, label=f'State {state}')
-plt.title('Distribution of Cycle Times by Cycle State')
-plt.xlabel('Cycle Time (ms)')
-plt.ylabel('Frequency')
-plt.legend()
-plt.grid(True)
-plt.show()
+    # plt.show()
+    plt.savefig(save_bar)
+
+    # Box plot of cycle times by cycle state
+    plt.figure(figsize=(12, 8))
+    i = 0
+    for state in df['Cycle State'].unique():
+        i += 1
+        subset = df[df['Cycle State'] == state]
+        bg_colors = ['lightblue', 'lightgreen']
+        colors = ['blue', 'green']
+        plt.boxplot(subset['Cycle Time ms'], positions=[i], widths=0.4, patch_artist=True, boxprops=dict(facecolor=bg_colors[i % len(bg_colors)]), medianprops=dict(color=colors[i % len(colors)]), showfliers=False)
+    plt.xticks(range(1, len(df['Cycle State'].unique()) + 1), [state_dict[state] for state in df['Cycle State'].unique()])
+    plt.title(f'{scenario}: Box Plot of Cycle Times by Cycle State')
+    plt.xlabel('Cycle State')
+    plt.ylabel('Cycle Time (ms)')
+    plt.grid(True)
+    # plt.show()
+    plt.savefig(save_box)
+
+if __name__ == '__main__':
+    files = ['1IO_1blinking/cssp_cycle_times_1_1blink.csv', '2IO_2blinking/cssp_cycle_times_2_2blink.csv', '8IO_0blinking/cssp_cycle_times_8_0blink_bis.csv', '8IO_4blinking/cssp_cycle_times_8_4blink.csv', '8IO_8blinking/cssp_cycle_times_8_8blink.csv']
+    saves_bar = ['1IO_1blinking/cssp/histogramm_1IO_1.png', '2IO_2blinking/cssp/histogramm_2IO_2.png', '8IO_0blinking/cssp/histogramm_8IO_0.png', '8IO_4blinking/cssp/histogramm_8IO_4.png', '8IO_8blinking/cssp/histogramm_8IO_8.png']
+    saves_box = ['1IO_1blinking/cssp/boxplot_1IO_1.png', '2IO_2blinking/cssp/boxplot_2IO_2.png', '8IO_0blinking/cssp/boxplot_8IO_0.png', '8IO_4blinking/cssp/boxplot_8IO_4.png', '8IO_8blinking/cssp/boxplot_8IO_8.png']
+    for i in range(len(files)):
+        cssp_cycle_analyzer(files[i], f'Scenario {i+1}', saves_bar[i], saves_box[i])
--- daughter_cycle_analyzer.py
+++ daughter_cycle_analyzer.py
@@ -1,55 +1,109 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def daughter_cycle_analyzer(file_path, scenario, save_bar, save_box):
+    # Load the CSV file
+    df = pd.read_csv(file_path)
+    df['Cycle Time ms'] = df['Cycle time'] * 1000
+    df['Request Time ms'] = df['Request time'] * 1000
+    # Analysis of cycle times by cycle state
+    state_stats = df.groupby('Read input')['Cycle Time ms'].describe()
+    request_stats = df.groupby('Read input')['Request Time ms'].describe()
 
-# Load the CSV file
-df = pd.read_csv('1IO_1blinking/cycle_time.csv')
-df['Cycle Time ms'] = df['Cycle time'] * 1000
-df['Request Time ms'] = df['Request time'] * 1000
-# Analysis of cycle times by cycle state
-state_stats = df.groupby('Read input')['Cycle Time ms'].describe()
-request_stats = df.groupby('Read input')['Request Time ms'].describe()
+    print("\nStatistics of Cycle Times by Cycle State:")
+    print(state_stats)
 
-print("\nStatistics of Cycle Times by Cycle State:")
-print(state_stats)
+    print("\nStatistics of Request Times by Request:")
+    print(request_stats)
 
-print("\nStatistics of Request Times by Request:")
-print(request_stats)
+    print("\nHighest cycle times:")
+    for state in df['Read input'].unique():
+        print(f"\nTop 10 highest cycle times for Cycle State {state}:")
+        print(df[df['Read input'] == state].nlargest(10, 'Cycle Time ms'))
 
-print("\nHighest cycle times:")
-for state in df['Read input'].unique():
-    print(f"\nTop 10 highest cycle times for Cycle State {state}:")
-    print(df[df['Read input'] == state].nlargest(10, 'Cycle Time ms'))
+    print("\nHighest cycle times:")
+    for state in df['Read input'].unique():
+        print(f"\nTop 10 highest cycle times for Cycle State {state}:")
+        print(df[df['Read input'] == state].nlargest(10, 'Request Time ms'))
 
-print("\nHighest cycle times:")
-for state in df['Read input'].unique():
-    print(f"\nTop 10 highest cycle times for Cycle State {state}:")
-    print(df[df['Read input'] == state].nlargest(10, 'Request Time ms'))
+    print("\nSum of cycle times:")
+    for state in df['Read input'].unique():
+        print(f"\nSum of cycle times for Cycle State {state}:")
+        print(df[df['Read input'] == state]['Cycle Time ms'].sum())
 
-print("\nSum of cycle times:")
-for state in df['Read input'].unique():
-    print(f"\nSum of cycle times for Cycle State {state}:")
-    print(df[df['Read input'] == state]['Cycle Time ms'].sum())
+    # Plot statistics for each cycle state separately
+    # for state in df['Read input'].unique():
+    #     plt.figure(figsize=(10, 6))
+    #     subset = df[df['Read input'] == state]
+    #     plt.hist(subset['Cycle Time ms'], bins=30, alpha=0.7, label=f'{"Read" if state else "Write"} requests')
+    #     plt.title(f'Distribution of Cycle Times for {"read" if state else "write"} requests')
+    #     plt.xlabel('Cycle Time (ms)')
+    #     plt.ylabel('Frequency')
+    #     plt.legend()
+    #     plt.grid(True)
+    #     plt.show()
 
-# Plot statistics for each cycle state separately
-for state in df['Read input'].unique():
-    plt.figure(figsize=(10, 6))
-    subset = df[df['Read input'] == state]
-    plt.hist(subset['Cycle Time ms'], bins=30, alpha=0.7, label=f'{"Read" if state else "Write"} requests')
-    plt.title(f'Distribution of Cycle Times for {"read" if state else "write"} requests')
-    plt.xlabel('Cycle Time (ms)')
+    # Plot statistics by cycle state for request times
+    # for state in df['Read input'].unique():
+    #     plt.figure(figsize=(10, 6))
+    #     subset = df[df['Read input'] == state]
+    #     plt.hist(subset['Request Time ms'], bins=30, alpha=0.7, label=f'{"Read" if state else "Write"} requests')
+    #     plt.title(f'Distribution of Request Times for {"read" if state else "write"} requests')
+    #     plt.xlabel('Cycle Time (ms)')
+    #     plt.ylabel('Frequency')
+    #     plt.legend()
+    #     plt.grid(True)
+    #     plt.show()
+        # Plot all cycle states in one figure for cycle times
+        # plt.figure(figsize=(10, 6))
+        # for state in df['Read input'].unique():
+        #     subset = df[df['Read input'] == state]
+        #     plt.hist(subset['Cycle Time ms'], bins=30, alpha=0.7, label=f'{"Read" if state else "Write"} requests')
+        # plt.title('Distribution of Cycle Times by Cycle State')
+        # plt.xlabel('Cycle Time (ms)')
+        # plt.ylabel('Frequency')
+        # plt.legend()
+        # plt.grid(True)
+        # plt.show()
+
+    # Plot all cycle states in one figure for request times
+    plt.figure(figsize=(12, 8))
+    for state in df['Read input'].unique():
+        subset = df[df['Read input'] == state]
+        plt.hist(subset['Cycle Time ms'], bins=30, alpha=0.5, label=f'{"Read" if state else "Write"} Cycle Time')
+        plt.hist(subset['Request Time ms'], bins=30, alpha=0.5, label=f'{"Read" if state else "Write"} Request Time')
+    plt.title(f'{scenario}: Distribution of Cycle and Request Times by Cycle State')
+    plt.xlabel('Time (ms)')
     plt.ylabel('Frequency')
     plt.legend()
     plt.grid(True)
-    plt.show()
-# Plot statistics by cycle state
-for state in df['Read input'].unique():
-    plt.figure(figsize=(10, 6))
-    subset = df[df['Read input'] == state]
-    plt.hist(subset['Request Time ms'], bins=30, alpha=0.7, label=f'{"Read" if state else "Write"} requests')
-    plt.title(f'Distribution of Request Times for {"read" if state else "write"} requests')
-    plt.xlabel('Cycle Time (ms)')
-    plt.ylabel('Frequency')
-    plt.legend()
+    # plt.show()
+    plt.savefig(save_bar)
+
+    plt.figure(figsize=(12, 8))
+    i = 0
+    for state in df['Read input'].unique():
+        i += 1
+        subset = df[df['Read input'] == state]
+        bg_colors = ['lightblue', 'lightgreen']
+        colors = ['blue', 'green']
+        bg_colors_req = ['moccasin', 'salmon']
+        colors_req = ['orange', 'red']
+        plt.boxplot(subset['Cycle Time ms'], positions=[i], widths=0.4, patch_artist=True, boxprops=dict(facecolor=bg_colors[i % len(bg_colors)]), medianprops=dict(color=colors[i % len(colors)]), showfliers=False)
+        plt.boxplot(subset['Request Time ms'], positions=[i+0.5], widths=0.4, patch_artist=True, boxprops=dict(facecolor=bg_colors_req[i % len(bg_colors_req)]), medianprops=dict(color=colors_req[i % len(colors_req)]), showfliers=False)
+    plt.xticks(range(1, len(df['Read input'].unique()) + 1), [f'{"Read" if state else "Write"}' for state in df['Read input'].unique()])
+    plt.title(f'{scenario}: Box Plot of Cycle and Request Times by Cycle State')
+    plt.xlabel('Cycle State')
+    plt.ylabel('Time (ms)')
+    plt.legend(['Read Cycle Time', 'Read Request Time', 'Write Cycle Time', 'Write Request Time'])
     plt.grid(True)
-    plt.show()
+    # plt.show()
+    plt.savefig(save_box)
+
+if __name__ == '__main__':
+    files = ['1IO_1blinking/cycle_time.csv', '2IO_2blinking/cycle_time.csv', '8IO_0blinking/cycle_time.csv', '8IO_4blinking/cycle_time.csv', '8IO_8blinking/cycle_time.csv']
+    scenarios = ['Scenario 1', 'Scenario 2', 'Scenario 3', 'Scenario 4', 'Scenario 5']
+    saves_bar = ['1IO_1blinking/daughter_board/histogramm_1IO_1.png', '2IO_2blinking/daughter_board/histogramm_2IO_2.png', '8IO_0blinking/daughter_board/histogramm_8IO_0.png', '8IO_4blinking/daughter_board/histogramm_8IO_4.png', '8IO_8blinking/daughter_board/histogramm_8IO_8.png']
+    saves_box = ['1IO_1blinking/daughter_board/boxplot_1IO_1.png', '2IO_2blinking/daughter_board/boxplot_2IO_2.png', '8IO_0blinking/daughter_board/boxplot_8IO_0.png', '8IO_4blinking/daughter_board/boxplot_8IO_4.png', '8IO_8blinking/daughter_board/boxplot_8IO_8.png']
+    for i in range(len(files)):
+        daughter_cycle_analyzer(files[i], scenarios[i], saves_bar[i], saves_box[i])
